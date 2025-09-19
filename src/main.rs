fe08147f85427c7b980a8dd6f9a94b07c53b3592
Varelion <Business.8508@gmail.com>
2025-09-19 14:28:32 -0400

    test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use gsm7::{encode, decode, encoded_len, is_gsm7_compatible, Gsm7Config};
+use lib::gsm7::{Gsm7Config, decode, encode, encoded_len, is_gsm7_compatible};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let text = "Hello {world} €!";
@@ -22,7 +22,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let encoded_with_replacement = gsm7::encode_with_config(emoji_text, &config)?;
     let decoded_with_replacement = decode(&encoded_with_replacement)?;
-    println!("With replacement: {} -> {}", emoji_text, decoded_with_replacement);
+    println!(
+        "With replacement: {} -> {}",
+        emoji_text, decoded_with_replacement
+    );
 
     Ok(())
 }
